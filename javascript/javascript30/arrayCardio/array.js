7f8b1363a74824462c86d89fc670ce885a8abc73
kimhyonseong <corea6524@gmail.com>
2022-05-23 15:09:01 +0900

    array example1

--- javascript/javascript30/arrayCardio/array.js
+++ javascript/javascript30/arrayCardio/array.js
@@ -23,40 +23,70 @@ const people = [
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
-let born1500 = inventors.filter(x => x.year >= 1500 && x.year <= 1600);
+const born1500 = inventors.filter(x => x.year >= 1500 && x.year <= 1600);
 console.table(born1500);
 born1500.forEach(x=>console.log(x.first+' '+x.last))
 
 // Array.prototype.map()
 // 2. Give us an array of the inventor first and last names
-let fullName = inventors.map(x=>`${x.first} ${x.last}`);
+const fullName = inventors.map(x=>`${x.first} ${x.last}`);
 console.log(fullName);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
-let sortP = inventors.sort((a,b)=>a.year - b.year);
+const sortP = inventors.sort((a,b)=>a.year - b.year);
 console.log(sortP);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live?
-let lives = inventors.reduce(a=>a.last,a.year)
+/* reduce는 forEach처럼 4개의 인수를 받을 수 있음
+누산기 - 값을 다 더해버림
+ex - forEach(function(element,index,array))
+ex - reduce(function(sum,currentValue,currentIndex,array),0) - 마지막 0은 초기값 설정
+1번째 인수 sum은 배열값들의 합
+2번째 인수 currentValue는 현재 배열의 값
+3번째 인수 currentIndex는 현재 배열의 키
+4번째 인수 array는 대상 배열
+맨 마지막 0은 초기값이며 배열의 첫번째 값을 대체함? 없으면 첫번째 요소 사용
+ */
+const lives = inventors.reduce((sum,a) => {
+    return sum + (a.passed-a.year)
+},0)
 console.log(lives);
 
 // 5. Sort the inventors by years lived
-
+const oldest = inventors.sort((a,b) => {
+    return -(a.passed - a.year) + (b.passed - b.year);
+})
+console.log(oldest[0]);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
-
 // const category = document.querySelector('.mw-category');
 // const links = Array.from(category.querySelectorAll('a'));
-// const de = links
-//             .map(link => link.textContent)
-//             .filter(streetName => streetName.includes('de'));
+// const de = links.map(link => link.textContent)
+//     .filter(x=>x.includes('de'));
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+const peopleSort = people.sort(function (a,b) {
+    // console.log(a.split(', ')[1][0]);
+    // console.log(b.split(', ')[1][0]);
+    // return a.split(', ')[0] > b.split(', ')[0] ? 1 : -1;
 
+    const [aLast, aFirst] = a.split(', ');
+    const [bLast, bFirst] = b.split(', ');
+
+    return aLast > bLast ? 1 : -1;
+})
+console.log(peopleSort);
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+const trans = data.reduce(function (obj,item) {
+    if (!obj[item]) obj[item] = 0;
+    obj[item]++;
+    return obj;
+},{})
+console.log(trans);
