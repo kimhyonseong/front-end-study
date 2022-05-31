02ebcfef5a5a51695e1d348f6a08597e5c881807
kimhyonseong <corea6524@gmail.com>
2022-05-31 17:40:23 +0900

    use map / reduce

---
+++ javascript/javascript30/18.Array reduce/arrayReduce.js
@@ -0,0 +1,20 @@
+const timeNodes = [...document.querySelectorAll('[data-time]')];
+
+const seconds = timeNodes
+    .map(node => node.dataset.time)
+    .map(timeCode => {
+        const [mins, secs] = timeCode.split(':').map(parseFloat);
+        // console.log(mins,secs,(mins * 60) + secs);
+        return (mins * 60) + secs;
+    })
+    .reduce((total, vidSeconds)=> total + vidSeconds);
+console.log('all second : '+seconds);
+
+let secondsLeft = seconds;
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+
+console.log(hours,mins,secondsLeft);
