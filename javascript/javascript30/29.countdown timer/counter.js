35de5374dab2485794da5deaebfd664edbdcee2f
kimhyonseong <corea6524@gmail.com>
2022-06-06 22:55:58 +0900

    time counter

---
+++ javascript/javascript30/29.countdown timer/counter.js
@@ -0,0 +1,42 @@
+let countdown;
+const timerDisplay = document.querySelector('.display_time-left');
+const endTime = document.querySelector('.display_end-time');
+
+function timer(seconds) {
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    //console.log({now, then});
+    displayTimeLeft(seconds);
+    displayEndTime(then)
+
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+        if (secondsLeft < 0) {
+            clearInterval(countdown);
+            return;
+        }
+
+        displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+
+// 남은 시간 보여주기
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${
+        remainderSeconds}`;
+    //console.log({minutes, remainderSeconds});
+    timerDisplay.textContent = display;
+}
+
+// 몇시에 끝나는지
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const adjustHour = hour > 12 ? hour - 12 : hour;
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${adjustHour}:${minutes < 10 ? '0' : ''}${
+        minutes}`;
+}
