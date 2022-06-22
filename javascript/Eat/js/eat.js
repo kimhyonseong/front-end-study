084d823912f1234bc1d2e9dc45d20c003a68e693
kimhyonseong <corea6524@gmail.com>
2022-06-22 15:50:04 +0900

    fix error / modify eventhandler

--- javascript/Eat/js/eat.js
+++ javascript/Eat/js/eat.js
@@ -1,21 +1,19 @@
 import jsonData from './food.json' assert {type:"json"};
 import * as json from "./jsonParse";
-import {foodTemplate} from "./jsonParse";
 const foodData = jsonData;
 
 // 초기 셋팅
-document.querySelector('.food-list').innerHTML = json.showFood(foodData);
 const foodList = document.querySelector('.food-list');
+foodList.innerHTML = json.showFood(foodData);
 const foodInfo = document.querySelector(".food-info");
 
 // 객체 선언
 const mainBox = document.querySelector('main');
-
 const foodName = document.getElementById("food");
+let foodNum = parseInt(window.localStorage.getItem("currentFood")) || 0;  // 현재 음식 인덱스
 
 const cate = document.querySelector('.category');
 const cateBtn = cate.querySelectorAll('label');
-const foods = document.querySelectorAll('.food');
 
 const loginBox = document.querySelectorAll('.login');
 const loginInputs = loginBox.item(0).querySelectorAll('input');
@@ -24,7 +22,7 @@ const loginBt = loginBox.item(0).querySelectorAll('.buttons>input');
 const loginForm = document.querySelector('form[class="loginForm"]');
 const registerForm = document.querySelector('form[class="registerForm"]');
 
-//클레스까지 스크롤
+//클래스까지 스크롤
 function scrollClass(clasNm) {
     const element = document.querySelector(`.${clasNm}`);
     window.scroll({
@@ -40,13 +38,22 @@ function toggleActive() {
     }
 }
 
-function activeFix() {
-    cateBtn.forEach(btn => {
+function removeActive() {
+    Array.from(this).forEach(ele => {
+        ele.classList.remove('active');
+    })
+}
+
+function activeFix(index) {
+    cateBtn.forEach((btn, i) => {
         btn.classList.remove('a-fix');
         btn.classList.remove('active');
+
+        if (i === index) {
+            btn.classList.add('a-fix');
+            btn.classList.add('active');
+        }
     })
-    this.classList.add('a-fix');
-    this.classList.add('active');
 }
 
 function colorStar(stars,num = 0) {
@@ -71,6 +78,9 @@ function fixStar(stars,num = 0) {
     // 로그인 이동
     if (getCookie('id') === null) {
         if (confirm('로그인이 필요합니다. 로그인하시겠습니까?')) {
+            // 현재 음식 인덱스 번호 지정
+            foodNum = 1;
+            window.localStorage.setItem("currentFood",`${foodNum}`);
             login();
             scrollClass('background');
         }
@@ -98,21 +108,32 @@ function loginAction(e) {
         const userId = loginForm.querySelector('input[name="user_id"]').value;
         const password = loginForm.querySelector('input[name="user_pw"]').value;
 
-        const userDataId = window.localStorage.getItem('id').split(',');
-        const userDataPw = window.localStorage.getItem('password').split(',');
-        const userDataNm = window.localStorage.getItem('name').split(',');
+        if (!valueEmpty(userId) || !valueEmpty(password)) {
+            alert("빈칸을 확인해주세요.");
+            return false;
+        }
 
-        let test = userDataId.indexOf(userId);
+        if (window.localStorage.getItem('id')) {
+            const userDataId = window.localStorage.getItem('id').split(',');
+            const userDataPw = window.localStorage.getItem('password').split(',');
+            const userDataNm = window.localStorage.getItem('name').split(',');
 
-        if (userDataPw[test] === password) {
-            setCookie('id',userDataId[test]);
-            setCookie('name',userDataNm[test]);
-            alert('로그인 완료.');
+            let test = userDataId.indexOf(userId);
+            console.log(test);
 
-            // 내가 전에 평가하던 페이지로 가기
-            location.reload();
+            if (userDataPw[test] === password) {
+                setCookie('id', userDataId[test]);
+                setCookie('name', userDataNm[test]);
+                alert('로그인 완료.');
+
+                // 내가 전에 평가하던 페이지로 가기
+                location.reload();
+            } else {
+                alert('아이디와 비밀번호가 일치하지 않습니다.');
+                return false;
+            }
         } else {
-            alert('아이디와 비밀번호가 일치하지 않습니다.');
+            alert("가입되지 않은 회원입니다.");
             return false;
         }
     } else {
@@ -164,7 +185,7 @@ function localSave(obj) {
     }
     if (typeof obj === 'object') {
         if (userIds) {
-            if (userIds.split(',').includes(obj.id)) {
+            if (userIds.split(',').includes(obj.userId)) {
                 alert('이미 사용 중인 아이디입니다.');
                 return false;
             }
@@ -173,7 +194,7 @@ function localSave(obj) {
             all.name = ','+window.localStorage.getItem('name');
         }
 
-        window.localStorage.setItem('id', `${obj.id}${all.id}`);
+        window.localStorage.setItem('id', `${obj.userId}${all.id}`);
         window.localStorage.setItem('password', `${obj.password}${all.password}`);
         window.localStorage.setItem('name', `${obj.name}${all.name}`);
     } else {
@@ -212,87 +233,129 @@ foodName.addEventListener('focusout',toggleLabel);
 // 카테고리 이벤트
 cateBtn.forEach(btn => btn.addEventListener('mouseenter',toggleActive))
 cateBtn.forEach(btn => btn.addEventListener('mouseleave',toggleActive))
-cateBtn.forEach(btn => btn.addEventListener('click',activeFix))
+cateBtn.forEach((btn,index) =>
+    btn.addEventListener('click',()=>activeFix(index)))
+activeFix(foodNum);
 
 // 음식 선택 이벤트
-function foodEventHandler(e) {
-    // console.log(e);
-    // console.log(e.target);
+function foodClickEvent(e) {
     if (e.target.classList.contains("food-name") && e.type === "click") {
-        let index = Array.from(document.querySelectorAll(".food-name")).findIndex((ele) => ele===e.target);
-        document.querySelector(".food-info").innerHTML = json.foodTemplate(foodData[index]);
+        const index = Array.from(document.querySelectorAll(".food-name")).findIndex((ele) => ele===e.target);
+
+        foodInfo.innerHTML = json.foodTemplate(foodData[index]);
+        removeActive.call(document.querySelectorAll(".food-name"));
         displayShow('food-info');
         scrollClass('food-info');
-    } 
-    // else if(e.target.classList.contains("food-name")) {
-    //     console.log(e);
-    //     console.log("enter");
-    //     toggleActive.call(e.target);
-    // }
+    }
 }
-foodList.addEventListener("click", foodEventHandler);
-foods.forEach(food =>
-    food.querySelector('.food-name').addEventListener('mouseenter',toggleActive))
-foods.forEach(food =>
-    food.querySelector('.food-name').addEventListener('mouseleave',toggleActive))
+
+let prevFoodList = foodList;
+function foodMousemoveEvent(e) {
+    //enter
+    if(e.target.classList.contains("food-name") && !prevFoodList.classList.contains("food-name")) {
+        toggleActive.call(e.target);
+    }
+
+    //leave
+    else if (!e.target.classList.contains("food-name") && prevFoodList.classList.contains("food-name")) {
+        toggleActive.call(prevFoodList);
+    }
+
+    else if(!e.target.classList.contains("food-name") && !prevFoodList.classList.contains("food-name")) {
+        removeActive.call(document.querySelectorAll(".food-name"));
+    }
+    prevFoodList = e.target;
+}
+foodList.addEventListener("click", foodClickEvent);
+foodList.addEventListener("mousemove", foodMousemoveEvent);
+
 
 // 별 평가 이벤트
 let prevFoodInfo = "NONE";
-function foodInfoEventHandler(e) {
-    // console.log(e.type);
-    // console.log(e.target.nodeName);
-
-    if (e.target.name === "star" && e.type === "click") {
-        const star = document.querySelectorAll("input[name='star']");
-        let index = Array.from(star).findIndex((ele) => ele===e.target);
-        fixStar(star,index);
-    }
-
+function starMousemoveEvent(e) {
     // leave
     if (prevFoodInfo.toLowerCase() === "label" && e.target.nodeName.toLowerCase() !== "label") {
         const star = document.querySelectorAll(".star");
         colorStar.call(window,star,-1);
     }
 
+    // enter
     if (e.target.nodeName.toLowerCase() === "label" && e.type === "mousemove") {
-        console.log(e.toElement.parentElement);
         const star = document.querySelectorAll(".star");
         let index = Array.from(star).findIndex((ele) => ele===e.toElement.parentElement);
         colorStar.call(window,star,index);
     }
     prevFoodInfo = e.target.nodeName;
 }
-foodInfo.addEventListener("click",foodInfoEventHandler);
-foodInfo.addEventListener("mousemove",foodInfoEventHandler);
+
+function starClickEvent(e) {
+    if (e.target.name === "star" && e.type === "click") {
+        const star = document.querySelectorAll("input[name='star']");
+        let index = Array.from(star).findIndex((ele) => ele===e.target);
+        fixStar(star,index);
+    }
+}
+foodInfo.addEventListener("click",starClickEvent);
+foodInfo.addEventListener("mousemove",starMousemoveEvent);
 
 // 로그인
 loginInputs.forEach(input => input.addEventListener('focus',toggleLabel));
 loginInputs.forEach(input => input.addEventListener('focusout',toggleLabel));
+loginInputs.forEach(input =>
+    input.addEventListener('keypress',(e) => {
+        if (e.code.toLowerCase() === "space") {
+            e.preventDefault();
+        }
+    }));
 loginBt.forEach(bt => bt.addEventListener('click', loginAction))
 
 // 회원가입
+function valueEmpty(value) {
+    // 비었으면 false
+    return value.trim() !== "";
+}
+function rexExp(value) {
+    // 특수문자 포함시 true
+    let specialStr = new RegExp(/[`~!@#$%^&*\[\]|'"()\-_+=,.\/\\?><]/,"gi");
+    return specialStr.test(value);
+}
+function validationUser(userInfo) {
+    // 유효성 검사 - 모두 통과시 true
+    if (typeof userInfo !== "object") {
+        alert('유효한 값이 아닙니다.');
+        return false;
+    }
+    if (rexExp(userInfo.userId) || rexExp(userInfo.name)) {
+        alert("아이디와 이름에는 특수문자 사용이 불가 합니다.");
+        return false;
+    }
+    if (!valueEmpty(userInfo.userId) || !valueEmpty(userInfo.name)) {
+        alert('빈칸을 확인해주세요.');
+        return false;
+    }
+}
 regiInputs.forEach(input => input.addEventListener('focus',toggleLabel));
 regiInputs.forEach(input => input.addEventListener('focusout',toggleLabel));
+regiInputs.forEach(input =>
+    input.addEventListener('keypress',(e) => {
+        if (e.code.toLowerCase() === "space") {
+            e.preventDefault();
+        }
+    }));
 registerForm.addEventListener('submit',(e)=>{
     e.preventDefault();
-    const userId = registerForm.querySelector('input[name="user_id"]');
-    const password = registerForm.querySelector('input[name="user_pw"]');
-    const name = registerForm.querySelector('input[name="user_name"]');
 
-    if (userId.value.trim() === '' ||
-        password.value.trim() === '' ||
-        name.value.trim() === ''
-    ) {
-        alert('빈칸을 확인해주세요');
+    const user = {
+        userId : registerForm.querySelector('input[name="user_id"]').value.trim(),
+        password : registerForm.querySelector('input[name="user_pw"]').value.trim(),
+        name : registerForm.querySelector('input[name="user_name"]').value.trim()
+    }
+
+    if(validationUser(user)) {
         return false;
     }
 
-    const obj = {
-        id : userId.value.trim(),
-        password : password.value.trim(),
-        name : name.value.trim()
-    }
-    if(localSave(obj)) {
+    if(localSave(user)) {
         alert('가입이 완료되었습니다.');
         allNone();
         displayShow('login');
