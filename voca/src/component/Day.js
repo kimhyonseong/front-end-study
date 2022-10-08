ad9519d8189af7b34a530663d1244aedd7528dfa
kimhyonseong <corea6524@gmail.com>
2022-10-08 15:51:39 +0900

    connect json-server

--- voca/src/component/Day.js
+++ voca/src/component/Day.js
@@ -1,20 +1,29 @@
 import data from "../db/data.json"
 import {useParams} from "react-router-dom";
+import Word from "./Word";
+import {useEffect, useState} from "react";
 
 export default function Day() {
     //const day = useParams().day;
     const {day} = useParams();
-    const wordList = data.words.filter(word => (word.day === Number(day)));
+    //const wordList = data.words.filter(word => (word.day === Number(day)));
+    const [words, setWords] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/words?day=${day}`)
+            .then(res=>res.json())
+            .then(data => {
+                setWords(data);
+            })
+    },[day])
+
     return (
         <>
             <h2>Day {day}</h2>
         <table>
             <tbody>
-            {wordList.map(word=>(
-                <tr key={word.id}>
-                    <td>{word.eng}</td>
-                    <td>{word.kor}</td>
-                </tr>
+            {words.map(word=>(
+                <Word word={word} key={word.id}/>
             ))}
             </tbody>
         </table>
--- voca/src/component/DayList.js
+++ voca/src/component/DayList.js
@@ -1,13 +1,26 @@
 import data from "../db/data.json"
 import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 export default function DayList() {
-    console.log(data);
-    return <ul className="list_day">
-        {data.days.map(data=>(
+    const [days,setDays] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:3001/days')
+            .then(res=>res.json())
+            .then(data => {
+                setDays(data);
+            })
+        console.log("Count change");
+    },[])
+
+    return <>
+    <ul className="list_day">
+        {days.map(data=>(
             <li key={data.id}>
                 <Link to={`/day/${data.day}`}>Day {data.day}</Link>
             </li>
         ))}
     </ul>
+    </>
 }
